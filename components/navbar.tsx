b1c15e4c894e65eff98bc3544985865993058cd3
Bijosh Shagar <bijosh22@gmail.com>
2024-01-10 15:57:24 +0530

    Navmenu animaation reveresd - not working

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { motion, useAnimation } from 'framer-motion';
 import Link from "next/link";
 import Image from "next/image";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
@@ -7,6 +6,7 @@ import { ReactNode, useState, useEffect, useRef } from "react";
 import logo from "@/public/logo.svg";
 import { cn } from "@/utils/cn";
 import { usePathname } from "next/navigation";
+import { motion, useAnimation } from 'framer-motion';
 
 interface Item {
   name: string;
@@ -24,20 +24,10 @@ function NavBar({ text }: NavBarProps) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
 
-  // Create animation controls
   const menuAnimationControls = useAnimation();
 
-  const handleMenuToggle = async () => {
+  const handleMenuToggle = () => {
     setIsMenuOpen(!isMenuOpen);
-    console.log("State : " + isMenuOpen);
-
-    if (isMenuOpen) {
-      // Animate out
-      await menuAnimationControls.start({ opacity: 0, x: -100 });
-    } else {
-      // Animate in
-      await menuAnimationControls.start({ opacity: 1, x: 0 });
-    }
   };
 
   useEffect(() => {
@@ -51,19 +41,15 @@ function NavBar({ text }: NavBarProps) {
       } else {
         navbarRef.current.style.opacity = "0";
         setIsMenuOpen(false);
-        // Set opacity using framer motion controls
-        menuAnimationControls.start({ opacity: 0, x: -100 });
       }
 
       previousScrollY.current = currentScrollY;
     };
 
-    console.log("run");
-
     window.addEventListener("scroll", handleScroll, { passive: true });
 
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [navbarRef, menuAnimationControls]);
+  }, [navbarRef]);
 
   useEffect(() => {
     setIsMenuOpen(false);
@@ -86,32 +72,18 @@ function NavBar({ text }: NavBarProps) {
       >
         <div className="p-5 flex relative flex-row sm:w-[50%] w-full bg-pgh-black sm:justify-evenly justify-between items-center border-b-2 border-pgh-gold">
           <div className="hidden sm:flex gap-10 font-zodiak">
-            {itemsLeft.map((item, index) => (
-              <motion.div
-                key={item.name}
-                initial={{ opacity: 0, x: -100 }}
-                animate={isMenuOpen ? { opacity: 1, x: 0 } : {}}
-                transition={{ duration: 0.5, delay: isMenuOpen ? index * 0.1 : 0 }}
-              >
-                <Link className="text-white" href={item.href}>
-                  {item.name}
-                </Link>
-              </motion.div>
+            {itemsLeft.map((item) => (
+              <Link key={item.name} className="text-white" href={item.href}>
+                {item.name}
+              </Link>
             ))}
           </div>
           <Image src={logo} width={150} alt="Presidency Group Hotels" />
           <div className="hidden sm:flex gap-10 font-zodiak">
-            {itemsRight.map((item, index) => (
-              <motion.div
-                key={item.name}
-                initial={{ opacity: 0, x: -100 }}
-                animate={isMenuOpen ? { opacity: 1, x: 0 } : {}}
-                transition={{ duration: 0.5, delay: isMenuOpen ? index * 0.1 : 0 }}
-              >
-                <Link className="text-white" href={item.href}>
-                  {item.name}
-                </Link>
-              </motion.div>
+            {itemsRight.map((item) => (
+              <Link key={item.name} className="text-white" href={item.href}>
+                {item.name}
+              </Link>
             ))}
           </div>
           <HamburgerMenuIcon
@@ -119,29 +91,27 @@ function NavBar({ text }: NavBarProps) {
             onClick={handleMenuToggle}
             color="white"
           />
-          <motion.ul
-            className={cn(
-              "absolute top-16 left-0 w-full text-white text-center translate-y-full bg-pgh-black flex-col py-3",
-              isMenuOpen && "flex"
-            )}
-            initial={{ opacity: 0, x: -100 }}
-            animate={menuAnimationControls}
+          {isMenuOpen && (
+            <motion.ul
+              className={cn(
+                "absolute top-16 left-0 w-full text-white text-center bg-pgh-black flex-col py-3"
+              )}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
             >
-            {itemsLeft.concat(itemsRight).map((item, index) => (
-              <motion.li
-                key={item.name}
-                className="border-pgh-gold py-2 font-zodiak"
-                initial={{ y: -20, opacity: 0 }}
-                animate={isMenuOpen ? { y: 0, opacity: 1 } : {}}
-                transition={{ duration: 0.5, delay: isMenuOpen ? index * 0.1 : 0 }}
-                whileHover={{ scale: 1.1 }}
-              >
-                <Link href={item.href} onClick={() => setIsMenuOpen(false)}>
-                  {item.name}
-                </Link>
-              </motion.li>
-            ))}
-          </motion.ul>
+              {itemsLeft.concat(itemsRight).map((item, index) => (
+                <motion.li
+                  key={item.name}
+                  className="border-pgh-gold py-2 font-zodiak"
+                  whileHover={{ scale: 1.1 }}
+                >
+                  <Link href={item.href} onClick={() => setIsMenuOpen(false)}>
+                    {item.name}
+                  </Link>
+                </motion.li>
+              ))}
+            </motion.ul>
+          )}
         </div>
       </nav>
     </>
